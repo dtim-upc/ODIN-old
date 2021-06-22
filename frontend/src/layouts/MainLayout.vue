8af0e655b44112bc7e6f1640e970edb59fcf3886
haross <javierfh1404@gmail.com>
2021-06-22 11:13:01 +0200

    dropdown menu

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -14,6 +14,41 @@
 
         <q-space />
 
+        <q-btn-dropdown stretch flat label="Manual bootstrapping">
+        <q-list>
+        <!--  <q-item-label header>Manual bootstrapping</q-item-label> -->
+          <q-item tabindex="0">
+            <q-item-section avatar>
+              <q-avatar icon="folder" color="secondary" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Global graph</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item tabindex="0">
+            <q-item-section avatar>
+              <q-avatar icon="folder" color="secondary" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label> Wrappers</q-item-label>
+            </q-item-section>
+          </q-item>
+        
+
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar icon="folder" color="secondary" text-color="white" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label> LAV mappings</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+
+
+      </q-btn-dropdown>
+
         <div class="q-gutter-sm row items-center no-wrap">
           <q-btn round dense flat color="white" :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
              @click="$q.fullscreen.toggle()"
