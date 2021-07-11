17af00d1a4b6f6df223127627221cdcb5a4bab24
Antoni Bergas <tonibergas.tb@gmail.com>
2021-07-11 02:53:06 +0200

    Can save graphs

--- frontend/src/components/graph/src/app/js/menu/saveGraphMenu.js
+++ frontend/src/components/graph/src/app/js/menu/saveGraphMenu.js
@@ -62,13 +62,13 @@ module.exports = function (graph) {
     saveGraphMenu.saveGraphicalGraph = function () {
         loadingModule=graph.options().loadingModule();
         exportMenu = graph.options().exportMenu();
+        console.log("loadingModule.currentGlobalGraph()")
         console.log(loadingModule.currentGlobalGraph());
-        /*$.ajax({
+        $.ajax({
             type: "PUT",
-            //url: odinApi+'/globalGraph/'+loadingModule.currentGlobalGraph().globalGraphID+'/graphicalGraph',
-            url: odinApi+'/globalGraph/60e42d04927d9433fd53d36c/graphicalGraph',
+            url: odinApi+'/globalGraph/'+loadingModule.currentGlobalGraph().id+'/graphicalGraph',
             data: exportMenu.getJson()
-        });*/
+        });
     };
 
     saveGraphMenu.hide = function (flag) {
