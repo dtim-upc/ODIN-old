592fa8c88fe5f67a3d2e775d7f4ffcc49aef3001
haross <javierfh1404@gmail.com>
2022-10-02 22:04:17 +0200

    cors

--- api/src/main/java/edu/upc/essi/dtim/odin/config/CorsConfiguration.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/config/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer
 {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/**").allowedOrigins("*")
                 .allowedMethods("GET", "POST","PUT","DELETE");
     }
 }
