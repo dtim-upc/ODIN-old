3024f102c8cb2e43eb359b0409205815bba36d87
Víctor Asenjo <victorasenj@gmail.com>
2023-05-22 17:33:01 +0200

    Update CorsConfiguration.java

--- api/src/main/java/edu/upc/essi/dtim/odin/config/CorsConfiguration.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/config/CorsConfiguration.java
@@ -12,8 +12,9 @@ public class CorsConfiguration implements WebMvcConfigurer
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST","PUT","DELETE")
-                .allowedHeaders("*");
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
