452d249ae55c456d70a7dd9c3e3a42cad92c02e4
Víctor Asenjo <victorasenj@gmail.com>
2023-05-22 19:45:34 +0200

    Update JpaOrmImplementation.java

--- api/src/main/java/edu/upc/essi/dtim/odin/NextiaStore/RelationalStore/JpaOrmImplementation.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/NextiaStore/RelationalStore/JpaOrmImplementation.java
@@ -57,7 +57,7 @@ public class JpaOrmImplementation implements ORMStoreInterface {
         try {
             String queryString = "SELECT d FROM " + entityClass.getSimpleName() + " d";
             em.getTransaction().begin();
-            //NOSONAR
+            @SuppressWarnings("squid:S2077")
             Query query = em.createQuery(queryString);
             objects = query.getResultList();
             em.getTransaction().commit();
@@ -107,7 +107,7 @@ public class JpaOrmImplementation implements ORMStoreInterface {
         try {
             String queryString = "DELETE FROM " + entityClass.getSimpleName() + " d";
             em.getTransaction().begin();
-            //NOSONAR
+            @SuppressWarnings("squid:S2077")
             Query query = em.createQuery(queryString);
             success = query.executeUpdate() > 0;
             em.getTransaction().commit();
