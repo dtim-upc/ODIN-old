65e84c6322f6b7e428cdcb1de11e3b143eb54e16
Víctor Asenjo <victorasenj@gmail.com>
2023-05-22 23:57:05 +0200

    odinApplication class tests

--- api/src/test/java/edu/upc/essi/dtim/odin/OdinApplicationTests.java
+++ api/src/test/java/edu/upc/essi/dtim/odin/OdinApplicationTests.java
@@ -5,6 +5,8 @@ import edu.upc.essi.dtim.odin.bootstrapping.SourceService;
 import edu.upc.essi.dtim.odin.project.ProjectController;
 import edu.upc.essi.dtim.odin.project.ProjectService;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class OdinApplicationTests {
+	private static final Logger logger = LoggerFactory.getLogger(OdinApplicationTests.class);
 
 	@Autowired
 	private ProjectController projectController;
@@ -24,6 +27,10 @@ class OdinApplicationTests {
 
 	@Test
 	void contextLoads() {
+		OdinApplication.main(new String[]{});
+		logger.info("Application started. Ready to receive API requests.");
+
+		assertNotNull(OdinApplication.class);
 		assertNotNull(projectController);
 		assertNotNull(projectService);
 		assertNotNull(sourceController);
