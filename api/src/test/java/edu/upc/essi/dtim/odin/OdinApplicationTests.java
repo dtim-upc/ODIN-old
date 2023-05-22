1428663f6cdceb34a5e73214757cdb448bb85633
Víctor Asenjo <victorasenj@gmail.com>
2023-05-22 19:27:02 +0200

    covering tests

--- api/src/test/java/edu/upc/essi/dtim/odin/OdinApplicationTests.java
+++ api/src/test/java/edu/upc/essi/dtim/odin/OdinApplicationTests.java
@@ -1,13 +1,33 @@
 package edu.upc.essi.dtim.odin;
 
+import edu.upc.essi.dtim.odin.bootstrapping.SourceController;
+import edu.upc.essi.dtim.odin.bootstrapping.SourceService;
+import edu.upc.essi.dtim.odin.project.ProjectController;
+import edu.upc.essi.dtim.odin.project.ProjectService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class OdinApplicationTests {
 
+	@Autowired
+	private ProjectController projectController;
+	@Autowired
+	private ProjectService projectService;
+	@Autowired
+	private SourceController sourceController;
+	@Autowired
+	private SourceService sourceService;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(projectController);
+		assertNotNull(projectService);
+		assertNotNull(sourceController);
+		assertNotNull(sourceService);
 	}
 
 }
---
+++ api/src/test/java/edu/upc/essi/dtim/odin/bootstrapping/SourceServiceTest.java
@@ -0,0 +1,114 @@
+package edu.upc.essi.dtim.odin.bootstrapping;
+
+import edu.upc.essi.dtim.NextiaCore.datasources.dataset.CsvDataset;
+import edu.upc.essi.dtim.NextiaCore.datasources.dataset.Dataset;
+import edu.upc.essi.dtim.NextiaCore.graph.Graph;
+import edu.upc.essi.dtim.NextiaCore.graph.LocalGraph;
+import edu.upc.essi.dtim.NextiaCore.graph.URI;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class SourceServiceTest {
+
+    @Autowired
+    private SourceService sourceService;
+
+    @Test
+    public void testReconstructFile() {
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file",
+                "test.csv",
+                "text/plain",
+                "test file content".getBytes()
+        );
+
+        String filePath = sourceService.reconstructFile(multipartFile);
+
+        assertEquals("..\\api\\dbFiles\\diskFiles\\", filePath.substring(0,25));
+    }
+
+    @Test
+    public void testExtractData() {
+        String filePath = "api/src/test/resources/csvTestFile.csv";
+        String datasetName = "Test Dataset";
+        String datasetDescription = "Description of the test dataset";
+
+        Dataset dataset = sourceService.extractData(filePath, datasetName, datasetDescription);
+
+        assertEquals(datasetName, dataset.getDatasetName());
+        assertEquals(datasetDescription, dataset.getDatasetDescription());
+        assertEquals(CsvDataset.class, dataset.getClass());
+        assertEquals(filePath, ((CsvDataset) dataset).getPath());
+    }
+
+    @Test
+    public void testTransformToGraph() {
+        // Create a dataset object
+        Dataset dataset = new CsvDataset("test", "Test Dataset", "Description", "../api/src/test/resources/csvTestFile.csv".replace("/", "\\"));
+
+        // Call the transformToGraph method
+        GraphModelPair graphModelPair = sourceService.transformToGraph(dataset);
+
+        // Add assertions to check the result
+        assertEquals(LocalGraph.class, graphModelPair.getGraph().getClass());
+        assertEquals(URI.class, graphModelPair.getGraph().getName().getClass());
+        assertEquals(dataset.getDatasetName(), graphModelPair.getGraph().getName().getURI());
+    }
+
+    @Test
+    public void testGenerateVisualSchema() {
+        // Create a graph object
+        Graph graph = new LocalGraph(null, new URI("Test Dataset"), new HashSet<>());
+
+        //TODO:
+        // Call the generateVisualSchema method
+        //String visualSchema = sourceService.generateVisualSchema(graph);
+        String visualSchema = "Expected visual schema";
+
+        // Add assertions to check the result
+        assertEquals("Expected visual schema", visualSchema);
+    }
+
+    @Test
+    public void testSaveGraphToDatabase() {
+        // Create a graph object
+        Graph graph = new LocalGraph(null, new URI("Test Dataset"), new HashSet<>());
+
+        // Call the saveGraphToDatabase method
+        boolean result = sourceService.saveGraphToDatabase(graph);
+
+        // Add assertions to check the result
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testAddDatasetIdToProject() {
+        String projectId = "1";
+        Dataset dataset = new CsvDataset(null, "Test Dataset", "Description", "path/to/file.csv");
+
+        // Call the addDatasetIdToProject method
+        sourceService.addDatasetIdToProject(projectId, dataset);
+
+        // Add assertions or perform necessary checks
+    }
+
+    @Test
+    public void testDeleteDatasetFromProject() {
+        String projectId = "1";
+        String datasetId = "dataset-id";
+
+        // Call the deleteDatasetFromProject method
+        sourceService.deleteDatasetFromProject(projectId, datasetId);
+
+        // Add assertions or perform necessary checks
+    }
+}
