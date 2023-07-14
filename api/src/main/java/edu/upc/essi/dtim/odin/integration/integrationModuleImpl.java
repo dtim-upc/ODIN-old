f9be6137106b9de7823bef641373b153d6658220
Víctor Asenjo <victorasenj@gmail.com>
2023-07-14 09:53:26 +0200

    implementing integrationModule

--- api/src/main/java/edu/upc/essi/dtim/odin/integration/integrationModuleImpl.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/integration/integrationModuleImpl.java
@@ -1,4 +1,22 @@
 package edu.upc.essi.dtim.odin.integration;
 
+import edu.upc.essi.dtim.NextiaCore.graph.CoreGraphFactory;
+import edu.upc.essi.dtim.NextiaCore.graph.Graph;
+import edu.upc.essi.dtim.NextiaDI;
+import edu.upc.essi.dtim.nextiadi.models.Alignment;
+
+import java.util.List;
+
 public class integrationModuleImpl implements integrationModuleInterface{
+    @Override
+    public Graph integrate(Graph graphA, Graph graphB, List<Alignment> alignments) {
+        Graph integratedGraph = CoreGraphFactory.createGraphInstance("normal");
+        NextiaDI n = new NextiaDI();
+
+        integratedGraph.setGraph(
+                n.Integrate(graphA.getGraph(), graphB.getGraph(), alignments)
+        );
+
+        return integratedGraph;
+    }
 }
--- api/src/main/java/edu/upc/essi/dtim/odin/integration/integrationModuleInterface.java
+++ api/src/main/java/edu/upc/essi/dtim/odin/integration/integrationModuleInterface.java
@@ -1,5 +1,10 @@
 package edu.upc.essi.dtim.odin.integration;
 
-public interface integrationModuleInterface {
+import edu.upc.essi.dtim.NextiaCore.graph.Graph;
+import edu.upc.essi.dtim.nextiadi.models.Alignment;
 
+import java.util.List;
+
+public interface integrationModuleInterface {
+    Graph integrate(Graph graphA, Graph graphB, List<Alignment> alignments);
 }
